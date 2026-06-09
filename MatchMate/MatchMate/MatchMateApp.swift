3e5b7e81d73926c9bcb01232cc97a4981ed2677e
Ruchit <ruchit.hungryminds@gmail.com>
2026-06-09 20:05:21 +0530

    Project setup clone repo

---
+++ MatchMate/MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Ruchit on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
